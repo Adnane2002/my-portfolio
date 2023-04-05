d9857c266142e32a546cbf8a3097c550fdff9b9d
Adnane2002 <adnanedouiab8@gmail.com>
2023-04-05 14:59:16 +0000

    changess

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import { Route, Routes,  } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import 'loaders.css/loaders.min.css';
 
 function App() {
-  
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <>
-
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projet" element={<Projects />} />
-      </Routes>
+      {isLoading ? (
+        <div className="loader-wrapper">
+          <div className="loader">
+            <div className="ball-pulse-sync">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projet" element={<Projects />} />
+          </Routes>
+        </>
+      )}
     </>
   );
 }
